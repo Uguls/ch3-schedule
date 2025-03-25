b2b3aede220ba944e1f758bd7b082a767d547bd5
Uguls <chobozaex@gmail.com>
2025-03-25 21:32:43 +0900

    fix: RequestMapping 추가

--- ch3-schedule/src/main/java/com/sparta/schedule/ch3_schedule/controller/ScheduleController.java
+++ ch3-schedule/src/main/java/com/sparta/schedule/ch3_schedule/controller/ScheduleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/schedules")
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
