e3790305a3874c238b61921e96f97f07838108f3
Uguls <chobozaex@gmail.com>
2025-03-25 19:05:23 +0900

    fix: 코드 가독성 좋게 수정

--- ch3-schedule/src/main/java/com/sparta/schedule/ch3_schedule/exception/GlobalExceptionHandler.java
+++ ch3-schedule/src/main/java/com/sparta/schedule/ch3_schedule/exception/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.sparta.schedule.ch3_schedule.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -35,10 +37,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
         Map<String, String> errors = new HashMap<>();
-        e.getBindingResult().getFieldErrors().forEach(error -> {
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
             errors.put(error.getField(), error.getDefaultMessage());
-        });
+        }
+
         return ResponseEntity.badRequest().body(errors);
+
     }
 
 }
